65f13b14e2d5c6965c7f4134d6c41a65278204d3
aliman <alimandeveloper@gmail.com>
2020-04-13 23:23:54 +0500

    ending settings django

--- shopdelivery/urls.py
+++ shopdelivery/urls.py
@@ -1,6 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.autodiscover()
+admin.site.site_header = 'Панель управления'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin_tools/', include('admin_tools.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('pages/', include('django.contrib.flatpages.urls')),
 ]
+
+if settings.DEBUG:
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
